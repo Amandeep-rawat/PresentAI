814bf56b484d1cd77e9d4f87cff8b18b631e2278
Amandeep-rawat <ar0466467@gmail.com>
2025-05-28 20:46:17 +0530

    fix lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-// import HomeFooter from "@/components/homeComponents/HomeFooter"
 import HomeNavbar from "@/components/homeComponents/HomeNavbar"
-import { useRouter } from "next/navigation"
 import HomeBody from "@/components/homeComponents/HomeBody"
 
 export default function LandingPage() {
