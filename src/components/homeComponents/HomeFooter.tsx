2324687c021b47c75e51eeac3e6caef4654c394e
Amandeep-rawat <ar0466467@gmail.com>
2025-06-02 14:37:27 +0530

    unplash using

--- src/components/homeComponents/HomeFooter.tsx
+++ src/components/homeComponents/HomeFooter.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Sparkles, Twitter, Github, Linkedin, Mail } from "lucide-react"
+import { Sparkles, Github, Linkedin, Mail } from "lucide-react"
 
 export default function HomeFooter() {
   return (
